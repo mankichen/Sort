f872e659707ceadecdb5c6a8a31135f994d8bb24
monkey <1401495306@qq.com>
2017-02-14 17:55:58 +0800

    simplified InsertSort

--- Sort/InsertSort.c
+++ Sort/InsertSort.c
@@ -9,25 +9,26 @@ Status InsertSort(int *array, int n)
 		if (array[i] > array[i + 1])
 		{
 			tmp = array[i + 1];//需要改变的元素的位置是 i+ 1
-			for (j = i; j >= 0; j--)
+			for (j = i + 1; j > 0 && array[j - 1] > tmp; j--)
 			{
-
+				array[j] = array[j - 1];
 
 				//如果前一个比后一个大，前一个后移
-				if (array[j] > tmp)
+				/*if (array[j] > tmp)
 				{
 					array[j] += array[j + 1];
 					array[j + 1] = array[j] - array[j + 1];
 					array[j] = array[j] - array[j + 1];
-				}
+				}*/
 
 				//如果前一个比后一个小 或者 j = 0，结束循环
-				if (0 == j || array[j - 1] < array[j])
+				/*if (0 == j || array[j - 1] < array[j])
 				{
 					array[j] = tmp;
 					break;
-				}
+				}*/
 			}
+			array[j] = tmp;
 		}
 	}
 	return true;
